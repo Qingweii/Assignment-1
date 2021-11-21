ec48c1c786ffbebade4713fba3690a1fe6b2067a
Qnwei <qingwei.wong5@gmail.com>
2021-11-21 12:19:36 +0800

    clearing the carousel script (redo)

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 //Owl carousel script//
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-})
 
 
 // qualification tab //
