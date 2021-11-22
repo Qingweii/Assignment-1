61a27ddc285163b5655d522e6d075bb9c95b4d06
Qnwei <qingwei.wong5@gmail.com>
2021-11-22 22:05:13 +0800

    javascript function (to toggle between hide and show for qualification)

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-//Owl carousel script
+//Carousel script
 
 
 
 // Qualification tab 
+
+var x = document.getElementById('work');
+var y = document.getElementById('education');
+
+function myFunction(){
+    x.style.display = 'block';
+    y.style.display = 'none';
+}
+
+function mysecondFunction(){
+    y.style.display = 'block';
+    x.style.display = 'none';
+}
